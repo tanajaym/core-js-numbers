31f87630516c583b9a540478aa23b6a50cbcc3ea
tanajaym <evasy2002>
2024-12-26 00:22:10 +0500

    feat: add getAngleBetweenVectors

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -95,25 +95,31 @@ function getLinearEquationRoot(a, b) {
 }
 // getLinearEquationRoot(1,8);
 
-/**
- * Returns an angle (in radians) between two vectors given by xi and yi,
- * coordinates in Cartesian plane.
- * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
- *
- * @param {number} x1
- * @param {number} y1
- * @param {number} x2
- * @param {number} y2
- * @return {number}
- *
- * @example:
- *   (1,0) (0,1)     => π/2
- *   (0,1) (0,-1)    => π
- *   (0,-1) (1,0)    => π/2
- *   (0,1) (0,1)     => 0
- */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns an angle (in radians) between two vectors given by xi and yi,
+//  * coordinates in Cartesian plane.
+//  * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
+//  *
+//  * @param {number} x1
+//  * @param {number} y1
+//  * @param {number} x2
+//  * @param {number} y2
+//  * @return {number}
+//  *
+//  * @example:
+//  *   (1,0) (0,1)     => π/2
+//  *   (0,1) (0,-1)    => π
+//  *   (0,-1) (1,0)    => π/2
+//  *   (0,1) (0,1)     => 0
+//  */
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dotProduct = x1*x2 + y1*y2; 
+  //скалярное произведение
+  const vectorLength1 = Math.sqrt(Math.pow(x1, 2)+Math.pow(y1, 2));
+  const vectorLength2 = Math.sqrt(Math.pow(x2, 2)+Math.pow(y2, 2));
+
+  const angle = Math.acos(dotProduct/(vectorLength1*vectorLength2) );
+  return angle;
 }
 
 /**
