38646b6e6d780f3c6a21124ea49aa26d62742323
tanajaym <evasy2002>
2024-12-26 15:55:09 +0500

    feat: add getFloatOnString

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -458,18 +458,18 @@ function isInteger(number) {
   return Number.isInteger(number);
 }
 
-/**
- * Returns a floating point number or, if the number cannot be parsed from the argument, returns NaN.
- *
- * @param {string} str
- * @return {number | NaN}
- *
- * @example:
- * '4.567abcdefgh' => 4.567
- * 'abcdefgh'      => NaN
- */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns a floating point number or, if the number cannot be parsed from the argument, returns NaN.
+//  *
+//  * @param {string} str
+//  * @return {number | NaN}
+//  *
+//  * @example:
+//  * '4.567abcdefgh' => 4.567
+//  * 'abcdefgh'      => NaN
+//  */
+function getFloatOnString(str) {
+  return Number.parseFloat(str);
 }
 
 /**
