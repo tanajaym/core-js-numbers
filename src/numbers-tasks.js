90d8d2ca27e2ff689dda464c1d7bcffac0b77c0b
tanajaym <evasy2002>
2024-12-26 16:27:15 +0500

    feat: add getSumOfNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -560,24 +560,24 @@ function roundToNearestInteger(number) {
 //  * -5.5 => -5
 //  */
 function getIntegerPartNumber(number) {
-  return Math.trunc(number);
-  
+  return Math.trunc(number); 
 }
 
-/**
- * Returns the sum of numbers.
- *
- * @param {number} x1
- * @param {number} x2
- * @param {number} x3
- * @returns {number}
- *
- * @example:
- * 1, 2, 3       => 6
- * 0.1, 0.2, 0.3 => 0.6
- */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns the sum of numbers.
+//  *
+//  * @param {number} x1
+//  * @param {number} x2
+//  * @param {number} x3
+//  * @returns {number}
+//  *
+//  * @example:
+//  * 1, 2, 3       => 6
+//  * 0.1, 0.2, 0.3 => 0.6
+//  */
+function getSumOfNumbers(x1, x2, x3) {
+  const sum  = x1+x2+x3;
+  return toFixed(1);
 }
 
 /**
