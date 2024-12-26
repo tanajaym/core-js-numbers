fa4a4188917a0be3af563f93d800cbcbe4fa22a0
tanajaym <evasy2002>
2024-12-26 13:01:22 +0500

    feat: toNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -225,23 +225,26 @@ function isPrime(n) {
   return n%2 !== 0;
 }
 
-/**
- * Tries to convert value to number and returns it if conversion was successful;
- * otherwise returns default value passed as a second argument.
- *
- * @param {any} value
- * @param {any} def
- * @return {number}
- *
- * @example
- *   toNumber(null, 0) => 0
- *   toNumber('test', 0) => 0
- *   toNumber('1', 0) => 1
- *   toNumber(42, 0) => 42
- *   toNumber(new Number(42), 0) => 42
- */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+// /**
+//  * Tries to convert value to number and returns it if conversion was successful;
+//  * otherwise returns default value passed as a second argument.
+//  *
+//  * @param {any} value
+//  * @param {any} def
+//  * @return {number}
+//  *
+//  * @example
+//  *   toNumber(null, 0) => 0
+//  *   toNumber('test', 0) => 0
+//  *   toNumber('1', 0) => 1
+//  *   toNumber(42, 0) => 42
+//  *   toNumber(new Number(42), 0) => 42
+//  */
+function toNumber(value, def) {
+  let res = 0;
+  if(Number(value) === isNaN(value)) res = def;
+  else res = value;
+  return res;
 }
 
 /**
