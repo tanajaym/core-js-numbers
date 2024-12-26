427ece097530c0ed94352adfedaabce82d7c73f0
tanajaym <evasy2002>
2024-12-26 15:19:01 +0500

    feat: add toExponential

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -364,18 +364,18 @@ function numberToStringInBase(number, base) {
   return number.toString(base);
 }
 
-/**
- * Returns a string representation of a number in exponential notation.
- *
- * @param {number} number
- * @param {number} fractionDigits
- * @return {string}
- *
- * @example:
- * 12345, 2    => '1.23e+4'
- */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns a string representation of a number in exponential notation.
+//  *
+//  * @param {number} number
+//  * @param {number} fractionDigits
+//  * @return {string}
+//  *
+//  * @example:
+//  * 12345, 2    => '1.23e+4'
+//  */
+function toExponential(number, fractionDigits) {
+  return number.toExponential(fractionDigits);
 }
 
 /**
