eee78c4638fc50cbc52285b53caf98a72d2f6860
tanajaym <evasy2002>
2024-12-26 16:09:09 +0500

    feat: add roundToLargestInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -519,18 +519,18 @@ function roundToSmallestInteger(number) {
   return Math.floor(number);
 }
 
-/**
- * Returns the largest integer greater than or equal to a given number.
- *
- * @param {number} number
- * @return {number}
- *
- * @example:
- * 5.1  => 6
- * -5.9 => -5
- */
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns the largest integer greater than or equal to a given number.
+//  *
+//  * @param {number} number
+//  * @return {number}
+//  *
+//  * @example:
+//  * 5.1  => 6
+//  * -5.9 => -5
+//  */
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
 }
 
 /**
