7c4cf42c1734640f7e59798089c9e225c9d1aba7
tanajaym <evasy2002>
2024-12-26 16:07:49 +0500

    feat: add roundToSmallestInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -505,18 +505,18 @@ function isSafeInteger(number) {
   return Number.isSafeInteger(number);
 }
 
-/**
- * Returns the smallest integer less than or equal to a given number.
- *
- * @param {number} number
- * @return {number}
- *
- * @example:
- * 5.9  => 5
- * -5.1 => -6
- */
-function roundToSmallestInteger(/* number */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns the smallest integer less than or equal to a given number.
+//  *
+//  * @param {number} number
+//  * @return {number}
+//  *
+//  * @example:
+//  * 5.9  => 5
+//  * -5.1 => -6
+//  */
+function roundToSmallestInteger(number) {
+  return Math.floor(number);
 }
 
 /**
