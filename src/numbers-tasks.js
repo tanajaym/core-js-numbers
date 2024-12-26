dc8b408881260fcaf6c9ce8b5d500636d9be5ea1
tanajaym <evasy2002>
2024-12-26 16:02:03 +0500

    feat: add isSafeInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -490,19 +490,19 @@ function getIntegerOnString(str, base) {
   return Number.parseInt(str, base);
 }
 
-/**
- * Returns whether a number is a safe integer.
- *
- * @param {number} number
- * @return {boolean}
- *
- * @example:
- * 10       => true
- * 3.5      => false
- * 2 ** 53  => false
- */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns whether a number is a safe integer.
+//  *
+//  * @param {number} number
+//  * @return {boolean}
+//  *
+//  * @example:
+//  * 10       => true
+//  * 3.5      => false
+//  * 2 ** 53  => false
+//  */
+function isSafeInteger(number) {
+  return Number.isSafeInteger(number);
 }
 
 /**
