8f46813fde2297e4973ea6568fc4806e5d11ae95
tanajaym <evasy2002>
2024-12-26 16:10:16 +0500

    feat: add roundToNearestInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -533,19 +533,19 @@ function roundToLargestInteger(number) {
   return Math.ceil(number);
 }
 
-/**
- * Returns the value of a number rounded to the nearest integer.
- *
- * @param {number} number
- * @return {number}
- *
- * @example:
- * 5.5  => 6
- * 5.4  => 5
- * -5.5 => -5
- */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns the value of a number rounded to the nearest integer.
+//  *
+//  * @param {number} number
+//  * @return {number}
+//  *
+//  * @example:
+//  * 5.5  => 6
+//  * 5.4  => 5
+//  * -5.5 => -5
+//  */
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**
