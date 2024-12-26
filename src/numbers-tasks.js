b61556e0ee87074d76492edd7ca83d79e1045aa5
tanajaym <evasy2002>
2024-12-26 16:16:36 +0500

    feat: add getIntegerPartNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -548,19 +548,20 @@ function roundToNearestInteger(number) {
   return Math.round(number);
 }
 
-/**
- * Returns the integer part of a number by removing any fractional digits.
- *
- * @param {number} number
- * @return {number}
- *
- * @example:
- * 5.5  => 5
- * 5.4  => 5
- * -5.5 => -5
- */
-function getIntegerPartNumber(/* number */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns the integer part of a number by removing any fractional digits.
+//  *
+//  * @param {number} number
+//  * @return {number}
+//  *
+//  * @example:
+//  * 5.5  => 5
+//  * 5.4  => 5
+//  * -5.5 => -5
+//  */
+function getIntegerPartNumber(number) {
+  return Math.trunc(number);
+  
 }
 
 /**
