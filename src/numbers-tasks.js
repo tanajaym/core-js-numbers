48a1ea6affa1bed5ba192380620de6d5e041a51e
tanajaym <evasy2002>
2024-12-26 15:42:24 +0500

    feat: add number

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -443,19 +443,19 @@ function isNumber(number) {
   else return false;
 }
 
-/**
- * Returns a boolean value indicating whether a number is an integer or not.
- *
- * @param {number} number
- * @return {boolean}
- *
- * @example:
- * 5    => true
- * 5.1  => false
- * '5'  => false
- */
-function isInteger(/* number */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns a boolean value indicating whether a number is an integer or not.
+//  *
+//  * @param {number} number
+//  * @return {boolean}
+//  *
+//  * @example:
+//  * 5    => true
+//  * 5.1  => false
+//  * '5'  => false
+//  */
+function isInteger(number) {
+  return Number.isInteger(number);
 }
 
 /**
