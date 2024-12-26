39fa86620a29dfaeaa159d8c0efd98d1f86e17fb
tanajaym <evasy2002>
2024-12-26 15:13:12 +0500

    feat: add numberToStringInBase

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -349,19 +349,19 @@ function getSine(num) {
   return Math.sin(num);
 }
 
-/**
- * Returns a string representation of a number in a specified base (radix).
- *
- * @param {number} number
- * @param {number} base
- * @return {string}
- *
- * @example:
- * 255, 16 => 'ff'
- * 2, 2    => '10'
- */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+// /**
+//  * Returns a string representation of a number in a specified base (radix).
+//  *
+//  * @param {number} number
+//  * @param {number} base
+//  * @return {string}
+//  *
+//  * @example:
+//  * 255, 16 => 'ff'
+//  * 2, 2    => '10'
+//  */
+function numberToStringInBase(number, base) {
+  return number.toString(base);
 }
 
 /**
